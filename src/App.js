3d641cfaec2321237f1906be2107cb19eb4c39a6
Utkarsh Baviskar <72340484+utkarsh-develop@users.noreply.github.com>
2023-03-11 05:32:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Restaurent from './component/Basics/Restaurent';
+
+const App = () => {
+  return (
+    <>
+      <Restaurent/>
+    </>
+  )
+}
+
+export default App;
